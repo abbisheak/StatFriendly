08d1430af44d0419a9b19504abfec74daeb020ef
abbi0714 <abbi0714@student.ubc.ca>
2024-10-08 00:09:23 -0700

    Implemented LongData

--- src/main/model/LongData.java
+++ src/main/model/LongData.java
@@ -2,33 +2,31 @@ package model;
 
 // Represents Integer data in a data set
 public class LongData implements Data<Long>{
+    private Long data;
 
     //Effects: data's value is given value
     public LongData(Long data){
-        // stub
+        this.data = data;
     }
 
     //Effects: returns true if given data matches this data
     //         false otherwise
     public Boolean compare(Long data) {
-        // stub
-        return false;
+        return this.data.equals(data);
     }
 
     //Effects: returns true if given data contains the same data
     //         as this, false otherwise
     public Boolean compare(Data<Long> data) {
-        // stub
-        return false;
+        return compare(data.getData());
     }
 
     public void setData(Long newData) {
-        // stub
+       data = newData;
     }
 
     public Long getData() {
-        // stub
-        return null;
+        return data;
     }
     
 }

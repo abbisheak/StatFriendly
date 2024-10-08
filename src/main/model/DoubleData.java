afe775578ededf591269c61407cdf09ef49b29f9
abbi0714 <abbi0714@student.ubc.ca>
2024-10-08 07:22:39 -0700

    Added specifications for DoubleData

---
+++ src/main/model/DoubleData.java
@@ -0,0 +1,35 @@
+package model;
+
+// Represents Rational data in a data set
+public class DoubleData implements Data<Double> {
+
+    //Effects: data's value is given value
+    public DoubleData(){
+        // stub
+    }
+
+    //Effects: returns true if given data matches this data
+    //         up to 10 decimal places, false otherwise
+    public Boolean compare(Double data) {
+        // stub
+        return false;
+    }
+
+    //Effects: returns true if given data contains the same data
+    //         as this up to a 10 decimal point accuracy, 
+    //         false otherwise
+    public Boolean compare(Data<Double> data) {
+        // stub
+        return false;
+    }
+
+    public void setData(Double newData) {
+        // stub
+    }
+
+    public Double getData() {
+        // stub
+        return null;
+    }
+    
+}

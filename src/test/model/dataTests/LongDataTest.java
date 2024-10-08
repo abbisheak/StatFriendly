e4f65aaecc495fd3df93cdb1822ac7de72456d31
abbi0714 <abbi0714@student.ubc.ca>
2024-10-07 17:45:00 -0700

    Added tests for LongData

---
+++ src/test/model/dataTests/LongDataTest.java
@@ -0,0 +1,67 @@
+package model.dataTests;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import model.LongData;
+
+public class LongDataTest implements DataTest {
+    LongData testLargeLongData;
+    LongData testSmallLongData;
+
+    @BeforeEach
+    public void runBefore() {
+        testLargeLongData = new LongData(30022932132903L);
+        testSmallLongData = new LongData(-32L);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertTrue(30022932132903L == testLargeLongData.getData());
+        assertTrue(-32L == testSmallLongData.getData());
+    }
+
+    @Test
+    public void testCompareDataValue() {
+        assertTrue(testLargeLongData.compare(30022932132903L));
+        assertTrue(testSmallLongData.compare(-32L));
+
+        assertFalse(testLargeLongData.compare(5023023103021301203L));
+        assertFalse(testSmallLongData.compare(-2321L));
+    }
+
+    @Test
+    public void testCompareDataObject() {
+        LongData testTrueCompareLargeData = new LongData(30022932132903L);
+        LongData testTrueCompareSmallData = new LongData(-32L);
+
+        assertTrue(testLargeLongData.compare(testTrueCompareLargeData));
+        assertTrue(testSmallLongData.compare(testTrueCompareSmallData));
+
+        LongData testFalseCompareData = new LongData(5000000L);
+
+        assertFalse(testLargeLongData.compare(testFalseCompareData));
+        assertFalse(testSmallLongData.compare(testFalseCompareData));
+    }
+
+    @Test
+    public void testSetData() {
+        assertTrue(30022932132903L == testLargeLongData.getData());
+        assertTrue(-32L == testSmallLongData.getData());
+
+        testLargeLongData.setData(-983423434982L);
+        testSmallLongData.setData(932L);
+
+        assertTrue(-983423434982L == testLargeLongData.getData());
+        assertTrue(932L == testSmallLongData.getData());
+    }
+
+    @Test
+    public void testGetData() {
+        assertTrue(30022932132903L == testLargeLongData.getData());
+        assertTrue(-32L == testSmallLongData.getData());
+    }
+    
+}

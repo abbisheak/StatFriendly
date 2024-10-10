57feb67a47b8490edd14978d14a6c1180e484042
abbi0714 <abbi0714@student.ubc.ca>
2024-10-10 16:51:03 -0700

    Specified DataSet as an Abstract Class instead of a Class

--- src/main/model/DataSet.java
+++ src/main/model/DataSet.java
@@ -3,117 +3,126 @@ package model;
 import java.util.HashSet;
 import java.util.Iterator;
 
-// Represents a data set for a single concept
-public class DataSet<T, W> {
-    private HashSet<W> dataSet;
-    private Boolean isSample;
-    private Double mean;
-    private T median;
-    private T mode;
-    private T max;
-    private T min;
-    private Double standardDeviation;
+// Represents a data set for a specific concept
+public abstract class DataSet<T, W> {
 
-
-    // Requires: The generic used for W must be T: W<T>
-    // Effects: creates an empty data set with no elements in in it; if isSample
-    // sets makes DataSet a sample DataSet, otherwise a population dataSet
+    // Requires: The generic used for W must be T - W<T>
+    // Effects: creates an empty data set with no elements in it; 
+    // if isSample makes DataSet a sample DataSet, 
+    // otherwise a population DataSet
     public DataSet(Boolean isSample) {
-        dataSet = new HashSet<>();
-        this.isSample = isSample; 
     }
 
     // Modifies: this
-    // Effects: If data != null AND !contains(data) add to set and recalculate
-    // statistical information, otherwise do nothing;
+    // Effects: If data != null AND !contains(data) add data to set and
+    // recalculate statistical information, otherwise do nothing
     public void addData(W data) {
-        // stub
     }
 
     // Modifies: this
     // Effects: If set contains given data, then remove data from data set
     // otherwise do nothing
-    public void removeData(W data) {
-        // stub
+    public void removeData(W data){
     }
 
     // Effects: returns true if given data is in set, otherwise false
     public Boolean contains(W data) {
-        // stub
-        return false;
+        return null;
     }
 
     // Modifies: this
-    // Effects: removes all data entries in a data set
+    // Effects: removes all data entries from data set, and sets all 
+    // statistical calculations to null
     public void clear() {
-        // stub
     }
 
     // Effects: returns an iterator for the data set
     public Iterator<W> iterator() {
-        // stub
         return null;
     }
 
-    // Effects: returns the number of data entries in DataSet
+    // Effects: returns the number of data entries in the data set
     public int size() {
-        // stub
         return 0;
     }
 
-    // Requires: !dataSet.empty()
-    // Effects: returns true if DataSet is data for a sample population
+    // Requires: !dataSet.isEmpty()
+    // Effects: returns true if DataSet contains data for a sample population,
+    // else return false meaning DataSet contains data for a population
     public Boolean isSample() {
-        // stub
-        return false;
+        return null;
     }
 
-    // Requires: !dataSet.empty()
+    // Requires: !dataSet.isEmpty()
     public Double getMean() {
-        // stub
         return null;
     }
 
-    // Requires: !dataSet.empty()
-    // Effects: If size() is even, return the size()/2 greatest element
-    // of the set, else return the (size()-1)/2 greatest element of the 
-    // set
+    // Requires: !dataSet.isEmpty()
     public T getMedian() {
-        // stub
         return null;
     }
 
-    // Requires: !dataSet.empty()
-    // Effects: Returns the data value with greatest amount of occurences
-    // with a bias towards to the lesser data value
+    // Requires: !dataSet.isEmpty()
     public T getMode() {
-        // stub
         return null;
     }
 
-    // Requires: !dataSet.empty()
+    // Requires: !dataSet.isEmpty()
     public T getMax() {
-        // stub
         return null;
     }
 
-    // Requires: !dataSet.empty()
+    // Requires: !dataSet.isEmpty()
     public T getMin() {
-        // stub
         return null;
     }
 
-    // Requires: !dataSet.empty()
-    // Effects: If isSample, returns zero (base value) until size() > 1,
-    // then returns calculated standard deviation, else return standard
-    // deviation
+    // Requires: !dataSet.isEmpty()
     public Double getStandardDeviation() {
-        // stub
         return null;
     }
 
     public HashSet<W> getData() {
-        // stub
         return null;
     }
+
+    // Requires: !dataSet.isEmpty() AND data != null
+    // Modifies: this
+    // Effects: calculates the new mean, median, mode, max, min and
+    // standard deviation accordingly to present dataSet state
+    private void calculate(W data) {
+    }
+
+    // Modifies: this
+    // Effects: recalculates and sets the value of mean based on dataSet
+    // state
+    protected abstract void setMean();
+
+    // Modifies: this
+    // Effects: If size() is even, set median to the size()/2 greatest 
+    // element of the set, else set median to the (size()-1)/2 greatest 
+    // element of the set
+    protected abstract void setMedian();
+
+    // Modifies: this
+    // Effects: sets mode to the data value with greatest amount of occurences
+    // with a bias towards to the lesser data value, that is, if data values 
+    // have equivalent occurences, the lesser data value is chosen
+    protected abstract void setMode();
+
+    // Modifies: this
+    // Effects: if data.getData() > max, set max to data.getData(), otherwise 
+    // do nothing
+    protected abstract void setMax(W data);
+
+    // Modifies: this
+    // Effects: if data.getData() < min, set min to data.getData(), otherwise 
+    // do nothing
+    protected abstract void setMin(W data);
+
+    // Modifies: this
+    // Effects: if isSample() calculate sample standard deviation, else calculate
+    // population standard deviation 
+    protected abstract void setStandardDeviation();
 }

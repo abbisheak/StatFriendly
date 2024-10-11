2bfcee7f5abde47410f0da280bc74128038e3b6e
abbi0714 <abbi0714@student.ubc.ca>
2024-10-11 13:24:17 -0700

    Implemented StatCalculator

--- src/main/model/StatCalculator.java
+++ src/main/model/StatCalculator.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 // represents a calculator that performs computations for statistical
 // analysis
 public class StatCalculator {
+    private static final Double Z_SCORE_CI = 1.96;
+    private static final Double T_SCORE_ONE_TAIL = 1.699;
+    private static final Double Z_SCORE_ONE_TAIL = 1.645;
+    private static final Double T_SCORE_TWO_TAIL = 2.045;
+    private static final Double MAX_ACCURACY = 0.00001;
 
     public StatCalculator() {
     }
@@ -14,34 +19,63 @@ public class StatCalculator {
     // the confidence interval for a data set with respect to the
     // confidence level
     public ArrayList<Double> calculateConfidenceInterval(Double confidenceLevel, Double mean, Integer size, Double standardDeviation) {
-        // stub
-        return null;
+        Double marginOfError = Z_SCORE_CI * (standardDeviation / Math.sqrt(size));
+        Double upperBound = mean + marginOfError;
+        Double lowerBound = mean - marginOfError;
+
+        ArrayList<Double> confidenceInterval = new ArrayList<>();
+        confidenceInterval.add(lowerBound);
+        confidenceInterval.add(upperBound);
+
+        return confidenceInterval;
     }
 
     // Requires: confidenceLevel == 0.95 AND size == 30
-    // Effects: returns true if the given null hypothesis is failed to be
-    // rejected, otherwise return false indicating the null hypothesis is
-    // rejected
+    // Effects: returns true if the given deviation fails to reject the null
+    // hypothesis is failed to be rejected, otherwise return false indicating
+    // the null hypothesis is rejected. If isSample use sample method, else 
+    // use population method
     public Boolean oneTailLesserHypothesisTest(Double confidenceLevel, Double deviation, Double standardError, Boolean isSample) {
-        // stub
-        return null;
+        Double criticalValue;
+
+        if (isSample){
+            criticalValue = -T_SCORE_ONE_TAIL;
+        }
+        else{
+            criticalValue = -Z_SCORE_ONE_TAIL;
+        }
+
+        Double testStatistic = deviation / standardError;
+        return testStatistic > criticalValue;
     }
 
     // Requires: confidenceLevel == 0.95 AND size == 30
-    // Effects: returns true if the given null hypothesis is failed to be
-    // rejected, otherwise return false indicating the null hypothesis is
-    // rejected
+    // Effects: returns true if the given deviation fails to reject the null
+    // hypothesis is failed to be rejected, otherwise return false indicating
+    // the null hypothesis is rejected. If isSample use sample method, else 
+    // use population method
     public Boolean oneTailGreaterHypothesisTest(Double confidenceLevel, Double deviation, Double standardError, Boolean isSample) {
-        // stub
-        return null;
+        Double criticalValue;
+
+        if (isSample){
+            criticalValue = T_SCORE_ONE_TAIL;
+        }
+        else{
+            criticalValue = Z_SCORE_ONE_TAIL;
+        }
+
+        Double testStatistic = deviation / standardError;
+        return testStatistic < criticalValue;
     }
 
     // Requires: confidenceLevel == 0.95 AND size == 30
-    // Effects: returns true if the given null hypothesis is failed to be
-    // rejected, otherwise return false indicating the null hypothesis is
-    // rejected
+    // Effects: returns true if the given deviation fails to reject the null
+    // hypothesis is failed to be rejected, otherwise return false indicating
+    // the null hypothesis is rejected. If isSample use sample method, else 
+    // use population method
     public Boolean twoTailHypothesisTest(Double confidenceLevel, Double deviation, Double standardError, Boolean isSample) {
-        // stub
-        return null;
+        Double criticalValue = T_SCORE_TWO_TAIL;
+        Double testStatistic = deviation / standardError;
+        return testStatistic > -criticalValue && testStatistic < criticalValue;
     }
 }

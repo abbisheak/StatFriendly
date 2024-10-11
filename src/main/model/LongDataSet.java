e44423862194eb5083ac86ecfcd0dbd12ddea427
abbi0714 <abbi0714@student.ubc.ca>
2024-10-11 06:01:17 -0700

    Implemented LongDataSet

--- src/main/model/LongDataSet.java
+++ src/main/model/LongDataSet.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import model.data.LongData;
 
-public class LongDataSet extends DataSet<Long, LongData>{
+public class LongDataSet extends DataSet<Long, LongData> {
 
     // Effects: creates an empty data set with no elements in it;
     // if isSample makes DataSet a sample DataSet,
@@ -15,7 +18,13 @@ public class LongDataSet extends DataSet<Long, LongData>{
     // Effects: recalculates and sets the value of mean based on dataSet
     // state
     protected void setMean() {
-        // stub
+        Double sum = 0.0;
+
+        for (LongData data : dataSet) {
+            sum += data.getData();
+        }
+
+        mean = sum / dataSet.size();
     }
 
     // Modifies: this
@@ -23,7 +32,17 @@ public class LongDataSet extends DataSet<Long, LongData>{
     // element of the set, else set median to the (size()/2 + 1) greatest
     // element of the set
     protected void setMedian() {
-        // stub
+        ArrayList<Long> sortedList = sortedData(iterator());
+        int stop;
+
+        if (dataSet.size() % 2 == 0) {
+            stop = (dataSet.size() / 2) - 1;
+        } else {
+            stop = ((dataSet.size() + 1) / 2) - 1;
+        }
+
+        median = sortedList.get(stop);
+
     }
 
     // Modifies: this
@@ -31,28 +50,68 @@ public class LongDataSet extends DataSet<Long, LongData>{
     // with a bias towards to the lesser data value, that is, if data values
     // have equivalent occurences, the lesser data value is chosen
     protected void setMode() {
-        // stub
+        ArrayList<Long> sortedList = sortedData(iterator());
+        int occurences = 0;
+
+        for (LongData data : dataSet) {
+            int dataOccurences = Collections.frequency(sortedList, data.getData());
+            if (dataOccurences > occurences) {
+                occurences = dataOccurences;
+                mode = data.getData();
+            } else if (dataOccurences == occurences && data.getData() < mode) {
+                mode = data.getData();
+            }
+        }
     }
 
     // Modifies: this
     // Effects: if data.getData() > max OR max == null, set max to data.getData(),
     // otherwise do nothing
     protected void setMax(LongData data) {
-        // stub
+        if (max == null || data.getData() > max) {
+            max = data.getData();
+        }
     }
 
     // Modifies: this
     // Effects: if data.getData() < min OR min == null, set min to data.getData(),
     // otherwise do nothing
     protected void setMin(LongData data) {
-        // stub
+        if (min == null || data.getData() < min) {
+            min = data.getData();
+        }
     }
 
     // Modifies: this
     // Effects: if isSample() and size > 1 calculate sample standard deviation,
     // else calculate population standard deviation
     protected void setStandardDeviation() {
-        // stub
+        Double differenceSummation = 0.0;
+        if (isSample && dataSet.size() > 1) {
+            for (LongData data : dataSet) {
+                differenceSummation += Math.pow((data.getData() - mean), 2.0);
+            }
+            standardDeviation = Math.sqrt(differenceSummation / (dataSet.size() - 1));
+        } else {
+            for (LongData data : dataSet) {
+                differenceSummation += Math.pow((data.getData() - mean), 2.0);
+            }
+            standardDeviation = Math.sqrt(differenceSummation / dataSet.size());
+        }
     }
-    
+
+    // Effects: sorts data to be in increasing order in a list and returns that
+    // list
+    private ArrayList<Long> sortedData(Iterator<LongData> data) {
+        ArrayList<Long> DataList = new ArrayList<>();
+
+        while (data.hasNext()) {
+            DataList.add(data.next().getData());
+        }
+
+        Collections.sort(DataList);
+
+        return DataList;
+    }
+
 }

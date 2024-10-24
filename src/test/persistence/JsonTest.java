57247a4c0ffa8b7437aeb9e959003b3ad24ab729
abbi0714 <abbi0714@student.ubc.ca>
2024-10-24 13:35:15 -0700

    Added JsonTest for testing of persistence package

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,18 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import model.dataset.DoubleDataSet;
+import model.dataspace.DataVector;
+
+// Referenced from the JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+public class JsonTest {
+    
+    protected void checkThingy(String name, DoubleDataSet dataSet, DataVector dataVector) {
+        assertEquals(name, dataVector.getName());
+        assertEquals(dataSet, dataVector.getDataSet());
+    }
+
+}

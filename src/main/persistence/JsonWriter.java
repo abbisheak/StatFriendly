73c71b71294c2a62aad9d6ae40fb0a70c1a2832d
abbi0714 <abbi0714@student.ubc.ca>
2024-10-23 21:29:27 -0700

    Added specifications for JsonWriter

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,44 @@
+package persistence;
+
+import org.json.JSONObject;
+
+import model.DataSpace;
+
+import java.io.*;
+
+// Referenced from the JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+// Represents a writer that writes JSON representation of DataSpace to file
+public class JsonWriter {
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination){
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException{
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of data space to file
+    public void write(DataSpace dataSpace) {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        //stub
+    }
+}

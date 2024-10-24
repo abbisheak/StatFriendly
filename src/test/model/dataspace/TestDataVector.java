92e7975dd7d25fba04e7322e8254a3f5e3287720
abbi0714 <abbi0714@student.ubc.ca>
2024-10-24 16:27:12 -0700

    Added tests for DataVector

---
+++ src/test/model/dataspace/TestDataVector.java
@@ -0,0 +1,34 @@
+package model.dataspace;
+
+import model.data.DoubleData;
+import model.dataset.DoubleDataSet;
+import model.dataspace.*;
+
+import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDataVector {
+    private DataVector testDV;
+    private DoubleDataSet testDds;
+
+    @BeforeEach
+    public void runBefore(){
+        testDds = new DoubleDataSet(true);
+
+        for(int i = 0; i < 7; i++){
+            testDds.addData(new DoubleData(i * 1.0));
+        }
+
+        testDV = new DataVector("A", testDds);
+        
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals("A", testDV.getName());
+        assertEquals(testDds, testDV.getDataSet());
+    }
+}

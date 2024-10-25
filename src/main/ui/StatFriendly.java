0a477b260c6d3949df2a78c81c8423cd0076d771
abbi0714 <abbi0714@student.ubc.ca>
2024-10-25 12:35:14 -0700

    Implemented file saving and loading to user interface

--- src/main/ui/StatFriendly.java
+++ src/main/ui/StatFriendly.java
@@ -3,16 +3,23 @@ package ui;
 import model.StatCalculator;
 import model.data.DoubleData;
 import model.dataset.DoubleDataSet;
+import model.dataspace.DataSpace;
+import model.dataspace.DataVector;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.HashSet;
+import java.util.List;
 
 // Statistical analysis app, made with reference to TellerApp
 public class StatFriendly {
     private StatCalculator calculator = new StatCalculator();
-    private DoubleDataSet userDoubleDataSet;
     private Scanner input = new Scanner(System.in);
+    private DataSpace userDataSpace;
 
     // EFFECTS: runs the StatFriendly app
     public StatFriendly() {
@@ -21,25 +28,53 @@ public class StatFriendly {
 
     // MODIFIES: this
     // EFFECTS: processes whether the user would like to use the app
+    // and if they would, start a new file or load from a previous
     private void runStatFriendly() {
         Boolean quit = false;
         String userInput = null;
 
         while (!quit) {
             displayMenu();
-
             userInput = input.next();
             userInput = userInput.toLowerCase();
 
             if (userInput.equals("q")) {
                 quit = true;
-            } else {
-                init();
+            } else if (userInput.equals("l")) {
+                try {
+                    loadDataSpace();
+                } catch (IOException e) {
+                    System.out.println("This file does not exist");
+                }
+            } else if (userInput.equals("n")) {
+                newDataSpace();
             }
             input = new Scanner(System.in);
         }
 
         System.out.println("Goodbye!");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates a new data space project for user to provide data to
+    private void newDataSpace() {
+        System.out.println("Name your project:");
+        input = new Scanner(System.in);
+        String projectName = input.nextLine();
+        userDataSpace = new DataSpace(projectName);
+        init();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: if user has a previosly saved Data Space then loads last saved data
+    // space by user
+    private void loadDataSpace() throws IOException {
+        userDataSpace = new JsonReader("./data/userDataSpace.json").read();
+        if (userDataSpace.getName() != "") {
+            init();
+        } else {
+            System.out.println("You do not have a previous saved Data Space! Start a new Data Space.");
+        }
     }
 
     // MODIFIES: this
@@ -54,10 +89,13 @@ public class StatFriendly {
             displaySelection();
             userInput = input.next();
             userInput = userInput.toLowerCase();
-            if (userInput.equals("s")) {
-                continuousDataAnalysis(true);
-            } else if (userInput.equals("p")) {
-                continuousDataAnalysis(false);
+            if (userInput.equals("a")) {
+                addDataVector();
+            } else if (userInput.equals("v")) {
+                viewDataVectors();
+            } else if (userInput.equals("s")) {
+                save();
+                quit = true;
             } else if (userInput.equals("q")) {
                 quit = true;
             } else {
@@ -67,18 +105,64 @@ public class StatFriendly {
     }
 
     // MODIFIES: this
+    // EFFECTS: add's a data vector to user's data space
+    public void addDataVector() {
+        DataVector userDataVector;
+        DoubleDataSet userDataSet = new DoubleDataSet(false);
+        input = new Scanner(System.in);
+
+        System.out.println("What would you like to name this set of entries?");
+        String dataName = input.nextLine();
+
+        receiveEntries(userDataSet);
+        userDataVector = new DataVector(dataName, userDataSet);
+        userDataSpace.addDataVector(userDataVector);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: allow's user to view/modify the data vectors in their data space
+    public void viewDataVectors() {
+        input = new Scanner(System.in);
+        List<DataVector> userDataVectors = userDataSpace.getDataVectors();
+        DataVector userDataVector;
+        if (userDataVectors.size() != 0) {
+            System.out.println("Select which set of entries you would like to view");
+            for (int i = 0; i < userDataVectors.size(); i++) {
+                userDataVector = userDataVectors.get(i);
+                System.out.println(i + ": " + userDataVector.getName());
+            }
+            int userInput = input.nextInt();
+            viewDataVector(userDataVectors.get(userInput));
+        } else {
+            System.out.println("This project is empty! Add a Data Set first to view.\\");
+        }
+    }
+
+    // MODIFIES: userDataSpace.json
+    // EFFECTS: saves data space state onto a file
+    private void save() {
+        JsonWriter writer = new JsonWriter("./data/userDataSpace.json");
+        try {
+            writer.open();
+        } catch (IOException e) {
+            System.out.println("Something went wrong when saving your work");
+        }
+        writer.write(userDataSpace);
+        writer.close();
+    }
+
+    // MODIFIES: this
     // EFFECTS: Inputs user's data into data sets
-    private void continuousDataAnalysis(Boolean isSample) {
+    private void receiveEntries(DoubleDataSet userDataSet) {
         Boolean quit = false;
         Double userDataInput = null;
         String userInput = null;
-        userDoubleDataSet = new DoubleDataSet(isSample);
 
         while (!quit) {
             input = new Scanner(System.in);
             System.out.println("\nEnter your data (Must be a number):");
             userDataInput = input.nextDouble();
-            userDoubleDataSet.addData(new DoubleData(userDataInput));
+            userDataSet.addData(new DoubleData(userDataInput));
             input = new Scanner(System.in);
             System.out.println("Add more data?\n");
             System.out.println("(q) -> No");
@@ -90,20 +174,19 @@ public class StatFriendly {
                 quit = true;
             }
         }
-        analyzeDataDD();
     }
 
     // MODIFIES: this
-    // EFFECTS: allows user to view information about their data set
-    // and/or add elements to their data set
-    private void analyzeDataDD() {
+    // EFFECTS: allows user to view information about their data vector
+    // and/or add elements to their data vector's data set
+    private void viewDataVector(DataVector userDataVector) {
         Boolean quit = false;
         String userInput = null;
         int userSelection;
         while (!quit) {
             displayInformationOptions();
             userSelection = input.nextInt();
-            optionsDD(userSelection);
+            optionsDD(userSelection, userDataVector.getDataSet());
 
             System.out.println("\nView more?\n");
             System.out.println("(q) -> No");
@@ -118,25 +201,25 @@ public class StatFriendly {
 
     // MODIFIES: this
     // EFFECTS: based on user's input view's or add's to data set
-    private void optionsDD(int selection) {
+    private void optionsDD(int selection, DoubleDataSet userDataSet) {
         if (selection == 1) {
-            System.out.println("\nMean:" + userDoubleDataSet.getMean());
+            System.out.println("\nMean:" + userDataSet.getMean());
         } else if (selection == 2) {
-            System.out.println("\nMedian:" + userDoubleDataSet.getMedian());
+            System.out.println("\nMedian:" + userDataSet.getMedian());
         } else if (selection == 3) {
-            System.out.println("\nMode:" + userDoubleDataSet.getMode());
+            System.out.println("\nMode:" + userDataSet.getMode());
         } else if (selection == 4) {
-            System.out.println("\nMax:" + userDoubleDataSet.getMax());
+            System.out.println("\nMax:" + userDataSet.getMax());
         } else if (selection == 5) {
-            System.out.println("\nMin:" + userDoubleDataSet.getMin());
+            System.out.println("\nMin:" + userDataSet.getMin());
         } else if (selection == 6) {
-            System.out.println("\nStandard Deviation:" + userDoubleDataSet.getStandardDeviation());
+            System.out.println("\nStandard Deviation:" + userDataSet.getStandardDeviation());
         } else if (selection == 7) {
-            iterate();
+            iterate(userDataSet);
         } else if (selection == 8) {
             ArrayList<Double> confidenceLevel = calculator.calculateConfidenceInterval(0.95,
-                    userDoubleDataSet.getMean(),
-                    userDoubleDataSet.getData().size(), userDoubleDataSet.getStandardDeviation());
+                    userDataSet.getMean(),
+                    userDataSet.getData().size(), userDataSet.getStandardDeviation());
             System.out
                     .println("Confidence Interval:[" + confidenceLevel.get(0) + "," + confidenceLevel.get(1) + "]");
         }
@@ -145,8 +228,8 @@ public class StatFriendly {
     // MODIFIES: this
     // EFFECTS: User can either add an element to the data set or
     // view each element of their data set
-    private void iterate() {
-        HashSet<DoubleData> datas = userDoubleDataSet.getData();
+    private void iterate(DoubleDataSet userDataSet) {
+        HashSet<DoubleData> datas = userDataSet.getData();
 
         iterateMenu();
         int userSelection;
@@ -154,7 +237,7 @@ public class StatFriendly {
         userSelection = input.nextInt();
 
         if (userSelection == 1) {
-            addElement();
+            addElement(userDataSet);
         } else if (userSelection == 2) {
             for (DoubleData d : datas) {
                 System.out.println(d.getData());
@@ -164,27 +247,32 @@ public class StatFriendly {
 
     // MODIFIES: this
     // EFFECTS: adds user's input to data set
-    private void addElement() {
+    private void addElement(DoubleDataSet userDataSet) {
         System.out.println("Enter the value of the data you would like to add:");
         input = new Scanner(System.in);
-        userDoubleDataSet.addData(new DoubleData(input.nextDouble()));
+        userDataSet.addData(new DoubleData(input.nextDouble()));
     }
 
-    // EFFECTS: Asks user whether they would like to use the app
+    // EFFECTS: Asks user whether they would like to use the app by starting a
+    // new data space, or loading a previous data space, or if would like to quit
     private void displayMenu() {
         System.out.println("Welcome to StatFriendly!");
         System.out.println("\nSelect from:\n");
-        System.out.println("(y) -> Start statistical analysis");
+        System.out.println("(n) -> Start new Data Space");
+        System.out.println("(l) -> Load last Data Space");
         System.out.println("(q) -> Quit");
     }
 
-    // EFFECTS: Asks user to specify the data they would be working with, or quit
+    // EFFECTS: Asks user whether would like to add data sets to their data space,
+    // view data sets in their data space, save their data space onto a file and
+    // quit, or quit without saving
     private void displaySelection() {
-        System.out.println("What type of data would you be working with?");
+        System.out.println("What would you like to do with your Data Space");
         System.out.println("\nSelect from:");
-        System.out.println("(s) -> Continuous sample data");
-        System.out.println("(p) -> Continuous population data");
-        System.out.println("(q)  -> Quit");
+        System.out.println("(a) -> add a Data Set to your Data Space");
+        System.out.println("(v) -> view a Data Set in your Data Space");
+        System.out.println("(s) -> Save and Quit");
+        System.out.println("(q) -> Quit");
     }
 
     // EFFECTS: Asks user what they would like to do with their processed data

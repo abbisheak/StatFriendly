af7782f93d525af049aac4cd2569c178fbba0f05
abbi0714 <abbi0714@student.ubc.ca>
2024-11-21 19:40:14 -0800

    Added tests for getNames method

--- src/test/model/dataspace/TestDataSpace.java
+++ src/test/model/dataspace/TestDataSpace.java
@@ -4,11 +4,15 @@ package model.dataspace;
 import model.data.DoubleData;
 import model.dataset.DoubleDataSet;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class TestDataSpace {
     DataSpace testDataSpace;
@@ -22,16 +26,33 @@ public class TestDataSpace {
         dv2 = new DataVector("2", new DoubleDataSet(false));
         dv3 = new DataVector("3", new DoubleDataSet(false));
         testDataSpace = new DataSpace("Test");
-        testDataSpace.addDataVector(dv1);
-        testDataSpace.addDataVector(dv2);
-        testDataSpace.addDataVector(dv3);
     }
 
     @Test
     public void testGetDataVector(){
+        testDataSpace.addDataVector(dv1);
+        testDataSpace.addDataVector(dv2);
+        testDataSpace.addDataVector(dv3);
+
         assertEquals(dv1, testDataSpace.getDataVector("1"));
         assertEquals(dv2, testDataSpace.getDataVector("2"));
         assertEquals(dv3, testDataSpace.getDataVector("3"));
         assertNull(testDataSpace.getDataVector("4"));
+    }
+
+    @Test
+    public void testGetNames(){
+
+        assertEquals(new ArrayList<>(), testDataSpace.getNames());
+
+        testDataSpace.addDataVector(dv1);
+        testDataSpace.addDataVector(dv2);
+        testDataSpace.addDataVector(dv3);
+        List<String> testList = new ArrayList<>();
+        testList.add("1");
+        testList.add("2");
+        testList.add("3");
+        assertEquals(testList, testDataSpace.getNames());
+
     }
 }
